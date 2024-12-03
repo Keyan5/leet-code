a87765458b68ddd2059880418a095a0451ad8ed3
Karthikeyan A <119607621+Keyan5@users.noreply.github.com>
2024-12-03 07:07:56 +0530

    Time: 23 ms (73.44%) | Memory: 78 MB (96.97%) - LeetSync

--- take-u-forward/2232-adding-spaces-to-a-string/adding-spaces-to-a-string.java
+++ take-u-forward/2232-adding-spaces-to-a-string/adding-spaces-to-a-string.java
@@ -1,10 +1,11 @@
 class Solution {
     public String addSpaces(String s, int[] spaces) {
         StringBuilder sb = new StringBuilder();
-        int spaceInd = 0, spacesLen = spaces.length;
-        int sLen = s.length();
-        for(int ind=0; ind<sLen; ind++) {
-            if(spaceInd < spacesLen && ind == spaces[spaceInd]) {
+        // since we know how much mem required, we can pre allocate it for efficency
+        sb.ensureCapacity(s.length() + spaces.length);
+        int spaceInd = 0;
+        for(int ind=0; ind<s.length(); ind++) {
+            if(spaceInd < spaces.length && ind == spaces[spaceInd]) {
                 sb.append(' ');
                 spaceInd++;
             }
