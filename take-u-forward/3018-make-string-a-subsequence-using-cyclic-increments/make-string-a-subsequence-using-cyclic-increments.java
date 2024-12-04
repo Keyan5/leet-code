701312a594df5fc989b38aece8c7abd25a35a573
Karthikeyan A <119607621+Keyan5@users.noreply.github.com>
2024-12-04 08:00:00 +0530

    Time: 5 ms (100.00%) | Memory: 45.5 MB (53.29%) - LeetSync

--- take-u-forward/3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.java
+++ take-u-forward/3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.java
@@ -1,16 +1,10 @@
 class Solution {
     public boolean canMakeSubsequence(String str1, String str2) {
-        int str2Ind = 0;
-        for(char c: str1.toCharArray()) {
-            if(c == str2.charAt(str2Ind) || getNextChar(c) == str2.charAt(str2Ind))
+        int str2Ind = 0, str2Len = str2.length();
+        for(char currChar: str1.toCharArray()) {
+            if(str2Ind < str2Len && (str2.charAt(str2Ind)-currChar+26)%26 <= 1)
                 str2Ind++;
-            if(str2Ind == str2.length())
-                return true;
         }
-        return false;
-    }
-
-    public char getNextChar(char c) {
-        return (char) ('a'+(c-'a'+1)%26);    // cyclic shift by 1
+        return str2Ind == str2Len;
     }
 }
