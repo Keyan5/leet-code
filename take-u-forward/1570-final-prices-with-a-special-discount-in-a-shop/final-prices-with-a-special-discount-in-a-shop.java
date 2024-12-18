441d6522fdd4e553af61500a0d9e91e0f6dbcf0c
Karthikeyan A <119607621+Keyan5@users.noreply.github.com>
2024-12-18 22:07:43 +0530

    Time: 3 ms (56.44%) | Memory: 43.9 MB (31.07%) - LeetSync

--- take-u-forward/1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
+++ take-u-forward/1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -1,12 +1,10 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
+        Stack<Integer> stack = new Stack();
         for(int idx=0; idx<prices.length; idx++) {
-            for(int jidx=idx+1; jidx<prices.length; jidx++) {
-                if(prices[idx]>=prices[jidx]) {
-                    prices[idx] -= prices[jidx];
-                    break;
-                }
-            }
+            while(!stack.isEmpty() && prices[stack.peek()] >= prices[idx])
+                prices[stack.pop()] -= prices[idx];
+            stack.push(idx);
         }
         return prices;
     }
