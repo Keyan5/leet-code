6fdc87843ae2087503a9f9672de77930d76a88d0
Karthikeyan A <119607621+Keyan5@users.noreply.github.com>
2024-12-30 08:24:56 +0530

    Time: 9 ms (55.37%) | Memory: 44 MB (51.01%) - LeetSync

--- take-u-forward/2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.java
+++ take-u-forward/2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.java
@@ -1,20 +1,17 @@
 class Solution {
-    private int low, high, zero, one;
-    private final HashMap<Integer, Integer> memo = new HashMap<>();
     private final int MOD = 1_000_000_007;
     public int countGoodStrings(int low, int high, int zero, int one) {
-        this.low = low;
-        this.high = high;
-        this.zero = zero;
-        this.one = one;
-        return dfs(0);
-    }
-
-    private int dfs(int len) {
-        if(memo.containsKey(len)) return memo.get(len);
-        if(len > high)  return 0;
-        long count = low <= len ? 1l : 0l;
-        memo.put(len, (int)((count + dfs(len+one) + dfs(len+zero)) % MOD));
-        return memo.get(len);
+        int[] table = new int[high+1];
+        table[0] = 1;
+        int count = 0;
+        for(int len = 0; len <= high; len++) {
+            if(len+zero <= high)
+                table[len+zero] = (int)((0L + table[len+zero] + table[len]) % MOD);
+            if(len+one <= high)
+                table[len+one] = (int)((0L + table[len+one] + table[len]) % MOD);
+            if(low <= len)
+                count = (int)((0L + count + table[len]) % MOD);
+        }
+        return count;
     }
 }
