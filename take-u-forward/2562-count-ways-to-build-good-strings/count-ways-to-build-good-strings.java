716bd5d0612bb87d2ee6c0a2e50a6cbaad924b96
Karthikeyan A <119607621+Keyan5@users.noreply.github.com>
2024-12-30 08:40:38 +0530

    Time: 6 ms (98.66%) | Memory: 43.4 MB (68.12%) - LeetSync

--- take-u-forward/2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.java
+++ take-u-forward/2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.java
@@ -1,16 +1,19 @@
 class Solution {
     private final int MOD = 1_000_000_007;
     public int countGoodStrings(int low, int high, int zero, int one) {
+        if(one > zero)
+            return countGoodStrings(low, high, one, zero);
         int[] table = new int[high+1];
         table[0] = 1;
         int count = 0;
-        for(int len = 0; len <= high; len++) {
-            if(len+zero <= high)
-                table[len+zero] = (int)((0L + table[len+zero] + table[len]) % MOD);
-            if(len+one <= high)
-                table[len+one] = (int)((0L + table[len+one] + table[len]) % MOD);
+        for(int len = 1; len <= high; len++) {
+            if(len >= zero)
+                table[len] += table[len-zero] + table[len-one];
+            else if(len >= one)
+                table[len] += table[len-one];
+            table[len] %= MOD;
             if(low <= len)
-                count = (int)((0L + count + table[len]) % MOD);
+                count = (count + table[len]) % MOD;
         }
         return count;
     }
