3fba3acd096426d81cab4ff566bdb588803c35c8
Karthikeyan A <119607621+Keyan5@users.noreply.github.com>
2025-01-02 07:44:34 +0530

    Time: 3 ms (100.00%) | Memory: 85.7 MB (63.33%) - LeetSync

--- take-u-forward/2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
+++ take-u-forward/2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
@@ -1,11 +1,10 @@
 class Solution {
     public int[] vowelStrings(String[] words, int[][] queries) {
         int[] prefixSum = new int[words.length];
-        prefixSum[0] = canCount(words[0]) ? 1 : 0;
-        for(int ind=1; ind<words.length; ind++) {
-            prefixSum[ind] += prefixSum[ind-1];
-            if(canCount(words[ind]))
-                prefixSum[ind]++;
+        int sum = 0;
+        for(int ind=0; ind<words.length; ind++) {
+            if(canCount(words[ind])) sum++;
+            prefixSum[ind] = sum;
         }
         int[] ans = new int[queries.length];
         for(int ind=0; ind<queries.length; ind++) {
