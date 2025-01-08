ef9b5e4af0cbdcb244b1164b2d97c375ea447229
Karthikeyan A <119607621+Keyan5@users.noreply.github.com>
2025-01-08 21:23:45 +0530

    Time: 2 ms (100.00%) | Memory: 42.1 MB (75.29%) - LeetSync

--- take-u-forward/3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.java
+++ take-u-forward/3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.java
@@ -14,7 +14,6 @@ class Solution {
         int len2 = str2.length();
         if(len1 > len2)
             return false;
-            
         for(int ind=0; ind<len1; ind++) {
             if(str1.charAt(ind) != str2.charAt(ind) || str1.charAt(ind) != str2.charAt(len2-len1+ind))
                 return false;
