91f4d209b2ed7f9c26deab046a6a27bb36519e27
Karthikeyan A <119607621+Keyan5@users.noreply.github.com>
2025-01-11 20:34:53 +0530

    Time: 5 ms (66.55%) | Memory: 45.7 MB (19.42%) - LeetSync

--- take-u-forward/1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
+++ take-u-forward/1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
@@ -2,12 +2,9 @@ class Solution {
     public boolean canConstruct(String s, int k) {
         if(s.length() < k)
             return false;
-        int[] freq = new int[26];
-        for(int ind=0; ind<s.length(); ind++)
-            freq[s.charAt(ind)-'a']++;
         int oddOccurence = 0;
-        for(int ind=0; ind<26; ind++)
-            oddOccurence += freq[ind]%2;
-        return oddOccurence <= k;
+        for(int ind=0; ind<s.length(); ind++)
+            oddOccurence ^= 1 << s.charAt(ind)-'a';
+        return Integer.bitCount(oddOccurence) <= k;
     }
 }
