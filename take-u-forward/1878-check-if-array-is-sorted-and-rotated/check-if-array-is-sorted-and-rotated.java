6c07036eb758c915a73f767e22a4a34562e5724f
Karthikeyan A <119607621+Keyan5@users.noreply.github.com>
2025-02-02 23:03:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (80.04%) - LeetSync

--- take-u-forward/1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ take-u-forward/1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,17 +1,10 @@
 class Solution {
     public boolean check(int[] nums) {
-        int arrEnd = 0;
-        int len = nums.length;
-        for(int ind=len-1; ind>0; ind--) {
-            if(nums[ind] < nums[ind-1]) {
-                arrEnd = ind;
-                break;
-            }
+        int cliffCount = 0, len = nums.length;
+        for(int ind=0; ind<len; ind++) {
+            if(nums[ind] > nums[(ind+1)%len])
+                cliffCount++;
         }
-        for(int ind=0; ind<arrEnd; ind++) {
-            if(nums[ind] < nums[ind == 0 ? len-1: ind-1])
-                return false;
-        }
-        return true;
+        return cliffCount <= 1;
     }
 }
