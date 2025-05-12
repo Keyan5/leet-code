d386c5583ef0b74ff6f5cad1a37b08952a7d02cb
Karthikeyan A <119607621+Keyan5@users.noreply.github.com>
2025-05-12 18:17:09 +0530

    Time: 2 ms (50.33%) | Memory: 47.9 MB (46.83%) - LeetSync

--- take-u-forward/547-number-of-provinces/number-of-provinces.java
+++ take-u-forward/547-number-of-provinces/number-of-provinces.java
@@ -1,39 +1,21 @@
 class Solution {
 
     public int findCircleNum(int[][] isConnected) {
-        Map<Integer, Set<Integer>> graph = constructGraph(isConnected);
         Set<Integer> visited = new HashSet<>();
         int count = 0;
-        for(int node: graph.keySet()) {
-            count += DFS(graph, visited, node);
+        for(int node=0; node<isConnected.length; node++) {
+            count += DFS(isConnected, visited, node);
         }
         return count;
     }
 
-    private Map<Integer, Set<Integer>> constructGraph(int[][] isConnected) {
-        Map<Integer, Set<Integer>> graph = new HashMap<>();
-        int size = isConnected.length;
-        for(int row = 0; row < size; row++) {    
-            for(int col = 0; col < size; col++) {
-                if(!graph.containsKey(row))
-                    graph.put(row, new HashSet<>());
-                if(!graph.containsKey(col))
-                    graph.put(col, new HashSet<>());
-                if(isConnected[row][col] == 1) {
-                    graph.get(row).add(col);
-                    graph.get(col).add(row);
-                }
-            }
-        }
-        return graph;
-    }
-
-    private int DFS(Map<Integer, Set<Integer>> graph, Set<Integer> visited, int node) {
-        if(visited.contains(node))
+    private int DFS(int[][] isConnected, Set<Integer> visited, int src) {
+        if(visited.contains(src))
             return 0;
-        visited.add(node);
-        for(int neighbor: graph.get(node))
-            DFS(graph, visited, neighbor);
+        visited.add(src);
+        for(int neighbor=0; neighbor<isConnected[src].length; neighbor++)
+            if(isConnected[src][neighbor]==1 && !visited.contains(neighbor))
+                DFS(isConnected, visited, neighbor);
         return 1;
     }
 
